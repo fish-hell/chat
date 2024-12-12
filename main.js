acabc2d87e20c295f35bbce2a75e01f1f7bba42e
fish-hell <156419134+fish-hell@users.noreply.github.com>
2024-12-12 16:33:13 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,174 @@
+function _0xd1c1(_0x4cbe1b,_0x205515){const _0x1dbcf0=_0x1dbc();return _0xd1c1=function(_0xd1c13a,_0x129128){_0xd1c13a=_0xd1c13a-0x83;let _0x270bfa=_0x1dbcf0[_0xd1c13a];if(_0xd1c1['QMTvaO']===undefined){var _0x317f88=function(_0x5e544c){const _0x507714='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';let _0x3658bb='',_0x1bad03='';for(let _0x31ca65=0x0,_0x1de2a5,_0x572215,_0x3f1216=0x0;_0x572215=_0x5e544c['charAt'](_0x3f1216++);~_0x572215&&(_0x1de2a5=_0x31ca65%0x4?_0x1de2a5*0x40+_0x572215:_0x572215,_0x31ca65++%0x4)?_0x3658bb+=String['fromCharCode'](0xff&_0x1de2a5>>(-0x2*_0x31ca65&0x6)):0x0){_0x572215=_0x507714['indexOf'](_0x572215);}for(let _0x446a30=0x0,_0x462922=_0x3658bb['length'];_0x446a30<_0x462922;_0x446a30++){_0x1bad03+='%'+('00'+_0x3658bb['charCodeAt'](_0x446a30)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(_0x1bad03);};_0xd1c1['LxhusZ']=_0x317f88,_0x4cbe1b=arguments,_0xd1c1['QMTvaO']=!![];}const _0x3ec24e=_0x1dbcf0[0x0],_0x12a7b6=_0xd1c13a+_0x3ec24e,_0x4181dc=_0x4cbe1b[_0x12a7b6];return!_0x4181dc?(_0x270bfa=_0xd1c1['LxhusZ'](_0x270bfa),_0x4cbe1b[_0x12a7b6]=_0x270bfa):_0x270bfa=_0x4181dc,_0x270bfa;},_0xd1c1(_0x4cbe1b,_0x205515);}function _0x1dbc(){const _0x4c845a=['mtnoBKrkyLK','mtiWode0ntLfz1j5BwS','ntyXAgv3zuTQ','ow1LBKXsyW','Ahr0Chm6lY9JAgf0ltf5zw4TzgvMyxvSDc1YDgrIlMzPCMvIyxnLAw8Uy29T','mJv0Aw9HBvi','y2HHDc0XEwvUlMzPCMvIyxnLC3rVCMfNzs5HCha','mtyYnZiYr2z3vfLw','mJG5mdj0zwTQtvO','y2HHDc0XEwvUlMzPCMvIyxnLyxbWlMnVBq','C2HPzNq','nJC1ndi5ug1Qz2PZ','mty1DfDyA3Pk','odC3odzwrhP0swW','otC3ntHRsez6yLK','mJK3nJKZr0nkve1L','otuWmJb4Dxb1Eum','mtjHrgPKEhi','ChvZAa','ndC5ntH2sMXdqu0','nhfVrufUtW','otC2mZq1mKjtyKLXBa','ndK1oda0ogzVD1Pgza','mKzQAgHurW','mta3nuzkqKf4Ba','mJu4nJC3mJaYmZm3','mte3mZuZmZjMtKvxB2O','quL6yvn5rg9JnNbMuKfTyvaXsLfAm0CTr1O1nfnKEwDmrtjultf3','mJy5nZj6wgznuu0','y2HHDc0XEwvU','n051Awn2BG'];_0x1dbc=function(){return _0x4c845a;};return _0x1dbc();}const _0x426749=_0xd1c1;(function(_0x31a2c2,_0x535c51){const _0x37bffe=_0xd1c1,_0x4ffde6=_0x31a2c2();while(!![]){try{const _0x34d153=parseInt(_0x37bffe(0x8a))/0x1*(parseInt(_0x37bffe(0x91))/0x2)+-parseInt(_0x37bffe(0x8c))/0x3*(-parseInt(_0x37bffe(0x87))/0x4)+parseInt(_0x37bffe(0x83))/0x5*(-parseInt(_0x37bffe(0x92))/0x6)+-parseInt(_0x37bffe(0x85))/0x7+-parseInt(_0x37bffe(0xa0))/0x8*(-parseInt(_0x37bffe(0x8d))/0x9)+-parseInt(_0x37bffe(0x9a))/0xa*(parseInt(_0x37bffe(0x96))/0xb)+parseInt(_0x37bffe(0x9f))/0xc;if(_0x34d153===_0x535c51)break;else _0x4ffde6['push'](_0x4ffde6['shift']());}catch(_0x229a46){_0x4ffde6['push'](_0x4ffde6['shift']());}}}(_0x1dbc,0xdb146));const _0x13ab4d=_0x55f6;function _0x55f6(_0x5e544c,_0x507714){const _0x3658bb=_0x4754();return _0x55f6=function(_0x1bad03,_0x31ca65){_0x1bad03=_0x1bad03-0x1b0;let _0x1de2a5=_0x3658bb[_0x1bad03];return _0x1de2a5;},_0x55f6(_0x5e544c,_0x507714);}(function(_0x572215,_0x3f1216){const _0x5ed19d=_0xd1c1,_0x446a30=_0x55f6,_0x462922=_0x572215();while(!![]){try{const _0x33f101=parseInt(_0x446a30(0x1b8))/0x1*(-parseInt(_0x446a30(0x1bf))/0x2)+parseInt(_0x446a30(0x1bc))/0x3*(-parseInt(_0x446a30(0x1bd))/0x4)+parseInt(_0x446a30(0x1b7))/0x5*(-parseInt(_0x446a30(0x1bb))/0x6)+parseInt(_0x446a30(0x1b4))/0x7*(-parseInt(_0x446a30(0x1b9))/0x8)+parseInt(_0x446a30(0x1be))/0x9+parseInt(_0x446a30(0x1b6))/0xa*(-parseInt(_0x446a30(0x1b5))/0xb)+parseInt(_0x446a30(0x1b2))/0xc*(parseInt(_0x446a30(0x1ba))/0xd);if(_0x33f101===_0x3f1216)break;else _0x462922[_0x5ed19d(0x9c)](_0x462922['shift']());}catch(_0x221d51){_0x462922['push'](_0x462922[_0x5ed19d(0x94)]());}}}(_0x4754,0x21e63));const firebaseConfig={'apiKey':_0x426749(0x86),'authDomain':_0x13ab4d(0x1b3),'databaseURL':_0x13ab4d(0x1b1),'projectId':_0x426749(0x88),'storageBucket':_0x426749(0x90),'messagingSenderId':_0x13ab4d(0x1b0),'appId':'1:258677202337:web:a869e51ddc776afedebfe0'};function _0x4754(){const _0x2d5de0=_0x426749,_0x499503=[_0x2d5de0(0x8f),_0x2d5de0(0xa1),'1983512RgsKSR',_0x2d5de0(0x8b),_0x2d5de0(0x9d),_0x2d5de0(0x95),_0x2d5de0(0x9e),_0x2d5de0(0x97),_0x2d5de0(0x98),_0x2d5de0(0x84),_0x2d5de0(0x8e),_0x2d5de0(0x9b),_0x2d5de0(0x93),_0x2d5de0(0x89),_0x2d5de0(0x99),'70OtgEtv'];return _0x4754=function(){return _0x499503;},_0x4754();}
+  
+firebase.initializeApp(firebaseConfig);
+  
+  const messagesRef = firebase.database().ref('messages');
+  const usersRef = firebase.database().ref('users');
+  
+  function setUserName() {
+    const inputname = prompt('ユーザー名を入力してください');
+    if (inputname) {
+    localStorage.setItem('userName',inputname);
+    window.location.reload();
+    }
+  }
+  
+  let userName = "名無し";
+  if (localStorage.getItem('userName')) {
+    userName = localStorage.getItem('userName');
+  }
+  
+  let format = '';
+  
+  function applyText() {
+      const messageInput = document.getElementById('message-input');
+      const boldChecked = document.querySelector('input[value="b"]').checked;
+      const italicChecked = document.querySelector('input[value="i"]').checked;
+      const strikethroughChecked = document.querySelector('input[value="s"]').checked;
+      const underlineChecked = document.querySelector('input[value="u"]').checked;
+  
+      const fontSize = document.querySelector('input[name="fontsize"]:checked').value;
+  
+      let Text1 = '';
+      if (boldChecked) {
+          Text1 = `[b]${Text1}`;
+      }
+      let Text2 = Text1;
+      if (italicChecked) {
+          Text2 = `[i]${Text2}`;
+      }
+      let Text3 = Text2;
+      if (strikethroughChecked) {
+          Text3 = `[s]${Text3}`;
+      }
+      let Text4 = Text3;
+      if (underlineChecked) {
+          Text4 = `[u]${Text4}`;
+      }
+  
+      let Text5 = Text4;
+      if (fontSize !== '0') {
+          Text5 = `[${fontSize}]${Text5}`;
+      }
+  
+      let chatColor = document.getElementById('colorinput').value;
+  
+      formattedMessage = `[${chatColor}]${Text5}`;
+      format = formattedMessage;
+      document.getElementById('textmenu').style.display = 'none'
+  }
+  
+  function sendMessage(message, sender) {
+    if (message.trim() === '') {
+      alert('メッセージを入力してください');
+      return;
+    } else if (message.length > 500) {
+      alert('文字数の上限を超えています');
+      return;
+    }
+
+    document.getElementById('message-input').value = '';
+  
+    const newMessage = {
+      text: format + message,
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      sender: sender || "名無し"
+    };
+  
+    messagesRef.push(newMessage);
+  }
+  
+  
+  function formatMessage(text) {
+    text = text.replace(/\[b\](.*?)(?=\[|$)/g, '<b>$1</b>');
+    text = text.replace(/\[i\](.*?)(?=\[|$)/g, '<em>$1</em>');
+    text = text.replace(/\[s\](.*?)(?=\[|$)/g, '<s>$1</s>');
+    text = text.replace(/\[u\](.*?)(?=\[|$)/g, '<u>$1</u>');
+    text = text.replace(/\[0\](.*?)(?=\[|$)/g, '<span style="font-size: 1em;">$1</span>');
+    text = text.replace(/\[1\](.*?)(?=\[|$)/g, '<span style="font-size: 1.7em;">$1</span>');
+    text = text.replace(/\[2\](.*?)(?=\[|$)/g, '<span style="font-size: 1.3em;">$1</span>');
+    text = text.replace(/\[3\](.*?)(?=\[|$)/g, '<span style="font-size: 0.7em;">$1</span>');
+    text = text.replace(/\[([a-zA-Z]+|#[0-9a-fA-F]{6})\](.*?)(?=\]|$)/g, '<span style="color:$1">$2</span>');
+  
+    return text;
+  }
+  
+  messagesRef.on('child_added', (snapshot) => {
+    const message = snapshot.val();
+    const time = new Date(message.timestamp).toLocaleString();
+  
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+  
+    const formattedText = formatMessage(message.text);
+  
+  
+    const youtubeRegex = /https?:\/\/(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/shorts\/)([a-zA-Z0-9_-]+)(?:#.*)?/;
+    const youtubeMatch = message.text.match(youtubeRegex);
+    const link = formattedText.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1" target="_blank">$1</a>');
+  
+    let content;
+    if (youtubeMatch) {
+      const videoId = youtubeMatch[1];
+      const embedHTML = `${link}<br><iframe width="320" height="180" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>上記のリンクをタップしてYouTubeに移動します</iframe>`;
+      content = embedHTML;
+    } else {
+      content = link;
+    }
+  
+    messageElement.innerHTML = `
+      <div class="meta-info">
+        <span class="sender-name">${message.sender}</span>
+        <span class="timestamp">${time}</span>
+      </div>
+      <div class="text">${content}</div>
+    `;
+  
+    document.getElementById('chat-messages').appendChild(messageElement);
+    document.getElementById('chat-messages').scrollTop = document.getElementById('chat-messages').scrollHeight;
+  });
+  
+  
+  
+  document.getElementById('textmenu-button').addEventListener('click', () => {
+      const textmenu = document.getElementById('textmenu');
+      textmenu.style.display = textmenu.style.display === 'none' || textmenu.style.display === '' ? 'block' : 'none';
+    });
+  
+  
+  document.getElementById('send-button').addEventListener('click', () => {
+    const messageInput = document.getElementById('message-input');
+    const message = messageInput.value;
+    sendMessage(message, userName);
+  });
+  
+  
+  document.getElementById('message-input').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      const messageInput = document.getElementById('message-input');
+      const message = messageInput.value;
+      sendMessage(message, userName);
+      messageInput.value = '';
+    }
+  });
+  
+  
+  function checkLimit() {
+    messagesRef.once('value', (snapshot) => {
+      const messages = snapshot.val();
+      const keys = Object.keys(messages || {});
+  
+      if (keys.length > 100) {
+  
+        const excessMessages = keys.slice(0, keys.length - 100);
+        excessMessages.forEach((key) => {
+          messagesRef.child(key).remove();
+        });
+      }
+    });
+  }
+  
+  
+  messagesRef.on('child_added', () => {
+    checkLimit();
+  });  
